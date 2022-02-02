b74ceda753ac810f23a2d58ecc3392a2bd48a3fe
DismissedLight <1686188646@qq.com>
2022-02-02 15:06:55 +0800

    add SG.Plugin.Injury.Calculator plugin

--- DGP.Genshin/Properties/AssemblyInfo.cs
+++ DGP.Genshin/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Windows;
 [assembly: AssemblyDescription("原神信息速查App")]
 [assembly: AssemblyCopyright("Copyright © DGP Studio 2021")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("2022.2.2.2")]
+[assembly: AssemblyVersion("2022.2.2.5")]
