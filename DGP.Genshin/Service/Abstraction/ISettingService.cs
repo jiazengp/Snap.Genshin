87960cfeab1954bad64e323b8ada2921cdfd337d
DismissedLight <1686188646@qq.com>
2022-02-21 15:47:39 +0800

    remove refed plugin

--- DGP.Genshin/Service/Abstraction/ISettingService.cs
+++ DGP.Genshin/Service/Abstraction/ISettingService.cs
@@ -25,6 +25,15 @@ namespace DGP.Genshin.Service.Abstraction
         /// 初始化设置服务，加载设置数据
         /// </summary>
         void Initialize();
+
+        /// <summary>
+        /// 使用定义设置设置值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="definition"></param>
+        /// <param name="value"></param>
+        /// <param name="notify"></param>
+        /// <param name="log"></param>
         void Set<T>(SettingDefinition<T> definition, object? value, bool notify = true, bool log = false);
 
         /// <summary>
